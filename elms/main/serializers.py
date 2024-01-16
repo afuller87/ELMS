9ce0f936d7eea868ad88705cbf777efcbea060c0
afuller87 <amyfuller19@gmail.com>
2024-01-15 21:56:43 -0500

    Created API for CRUD, Django REST Framework Authentication, Setup Migrate and Generate Token

--- elms/main/serializers.py
+++ elms/main/serializers.py
@@ -4,4 +4,4 @@ from . import models
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Teacher
-        fields = ['full_name', 'school_name', 'title', 'email', 'password', 'phone_number', 'address']
+        fields = ['id', 'full_name', 'school_name', 'title', 'email', 'password', 'phone_number', 'address']
