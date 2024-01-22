f0a6f04f3f62390aa90e33ab91d612b054574b58
afuller87 <amyfuller19@gmail.com>
2024-01-22 18:16:17 -0500

    Updated index html, Update courses, Created teachers on Home page

--- elms_frontend/src/components/Header.js
+++ elms_frontend/src/components/Header.js
@@ -1,19 +1,82 @@
-function Header() {
+import {Link} from 'react-router-dom';
+import {useState} from 'react';
+function Header(){
+    const [searchString,setsearchString]=useState({
+      'search':''
+    });
+    const teacherLoginStatus=localStorage.getItem('teacherLoginStatus');
+    const studentLoginStatus=localStorage.getItem('studentLoginStatus');
+
+    const handleChange=(event)=>{
+      setsearchString({
+          ...searchString,
+          [event.target.name]:event.target.value
+      });
+  }
+
+    const searchCourse = () =>{
+      if(searchString.search!=''){
+        window.location.href='/search/'+searchString.search
+      }
+      
+    }
+
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container">
-                <a className="navbar-brand" href="#">Enhanced Learning Management System</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav ms-auto">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        <a className="nav-link" href="#">Courses</a>
-                        <a className="nav-link" href="#">Teachers</a>
-                    </div>
-                </div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <Link className="navbar-brand" to="/">Learn Online</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <form class="d-flex">
+            <input name="search" onChange={handleChange} class="form-control me-2" type="search" placeholder="Search by course title" aria-label="Search" />
+            <button onClick={searchCourse} class="btn btn-warning" type="button">Search</button>
+          </form>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav ms-auto">
+              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+              <Link className="nav-link" to="/category">Categories</Link>
+              <Link className="nav-link" to="/all-courses">Courses</Link>
+              <li className="nav-item dropdown">
+                <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Teacher
+                </Link>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  {teacherLoginStatus!='true' && 
+                        <>
+                        <li><Link className="dropdown-item" to="/teacher-login">Login</Link></li>
+                        <li><Link className="dropdown-item" to="/teacher-register">Register</Link></li></>
+                  }
+                  {teacherLoginStatus=='true' &&
+                  <>
+                  <li><Link className="dropdown-item" to="/teacher-dashboard">Dashboard</Link></li>
+                  <li><Link className="dropdown-item" to="/teacher-logout">Logout</Link></li>
+                  </>
+                  }
+                </ul>
+              </li>
+              <li className="nav-item dropdown">
+                <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  User
+                </Link>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                {studentLoginStatus!=='true' && 
+                  <>
+                    <li><Link className="dropdown-item" to="/user-login">Login</Link></li>
+                    <li><Link className="dropdown-item" to="/user-register">Register</Link></li>
+                  </>
+                }
+                {studentLoginStatus==='true' &&
+                <>
+                  <li><Link className="dropdown-item" to="/user-dashboard">Dashboard</Link></li>
+                  <li><Link className="dropdown-item" to="/user-logout">Logout</Link></li>
+                </>
+                }
+                </ul>
+              </li>
             </div>
+          </div>
+        </div>
       </nav>
     );
   }
--- elms_frontend/src/components/Home.js
+++ elms_frontend/src/components/Home.js
@@ -1,77 +1,145 @@
-function Home() {
+import {Link} from 'react-router-dom';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+const baseUrl='http://127.0.0.1:8000/api';
+function Home(){
+  const [courseData,setCourseData]=useState([]);
+  const [popularcourseData,setpopularcourseData]=useState([]);
+  const [popularteacherData,setteacherData]=useState([]);
+  const [testimonialData,settestimonialData]=useState([]);
+    // Fetch courses when page load
+    useEffect(()=>{
+        // Fetch 4 courses
+        try{
+            axios.get(baseUrl+'/course/?result=4')
+            .then((res)=>{
+                setCourseData(res.data.results);
+            });
+        }catch(error){
+            console.log(error);
+        }
+        // fetch popular courses
+        try{
+          axios.get(baseUrl+'/popular-courses/?popular=1')
+          .then((res)=>{
+            setpopularcourseData(res.data);
+          });
+        }catch(error){
+            console.log(error);
+        }
+        // fetch popular teachers
+        try{
+          axios.get(baseUrl+'/popular-teachers/?popular=1')
+          .then((res)=>{
+            setteacherData(res.data);
+          });
+        }catch(error){
+            console.log(error);
+        }
+
+        // fetch student testimonial
+        try{
+          axios.get(baseUrl+'/student-testimonial/')
+          .then((res)=>{
+            settestimonialData(res.data);
+          });
+        }catch(error){
+            console.log(error);
+        }
+    },[]);
+
+    document.title='Home Page';
+
   return (
     <div className="container mt-4">
-      {/* Explore Courses */}
-      <h3 className="pb-1 mb-3">Explore Courses</h3>
-      <div className="row">
-        <div className="col-md-3">
+      {/* Latest Courses */}
+      <h3 className="pb-1 mb-4">Latest Courses <Link to="/all-courses" className="float-end btn btn-primary">See All</Link></h3>
+      <div className="row mb-4">
+      {courseData && courseData.map((course,index)=>
+        <div className="col-md-3 mb-4">
           <div className="card">
-            <a href="#"><img src="ELMSlogo.png" className="card-img-top" alt="..." /></a>
+            <Link to={`/detail/${course.id}`}><img src={course.featured_img} className="card-img-top" alt={course.title} /></Link>
             <div className="card-body">
-              <h5 className="card-title"><a href="#">Course Title</a></h5>
+              <h5 className="card-title"><Link to={`/detail/${course.id}`}>{course.title}</Link></h5>
             </div>
           </div>
         </div>
-        <div className="col-md-3">
-          <div className="card">
-            <a href="#"><img src="ELMSlogo.png" className="card-img-top" alt="..." /></a>
-            <div className="card-body">
-              <h5 className="card-title"><a href="#">Course Title</a></h5>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-3">
-          <div className="card">
-            <a href="#"><img src="ELMSlogo.png" className="card-img-top" alt="..." /></a>
-            <div className="card-body">
-              <h5 className="card-title"><a href="#">Course Title</a></h5>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-3">
-          <div className="card">
-            <a href="#"><img src="ELMSlogo.png" className="card-img-top" alt="..." /></a>
-            <div className="card-body">
-              <h5 className="card-title"><a href="#">Course Title</a></h5>
-            </div>
-          </div>
-        </div>
-        {/* End Explore Courses */}
-        {/* Featured Courses */}
-        <h3 className="pb-1 mt-5 mb-3">Featured Courses</h3>
-        <div className="col-md-3">
-          <div className="card">
-            <a href="#"><img src="ELMSlogo.png" className="card-img-top" alt="..." /></a>
-            <div className="card-body">
-              <h5 className="card-title"><a href="#">Course Title</a></h5>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-3">
-          <div className="card">
-            <a href="#"><img src="ELMSlogo.png" className="card-img-top" alt="..." /></a>
-            <div className="card-body">
-              <h5 className="card-title"><a href="#">Course Title</a></h5>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-3">
-          <div className="card">
-            <a href="#"><img src="ELMSlogo.png" className="card-img-top" alt="..." /></a>
-            <div className="card-body">
-              <h5 className="card-title"><a href="#">Course Title</a></h5>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-3">
-          <div className="card">
-            <a href="#"><img src="ELMSlogo.png" className="card-img-top" alt="..." /></a>
-            <div className="card-body">
-              <h5 className="card-title"><a href="#">Course Title</a></h5>
-            </div>
-          </div>
-        </div>
+        )}
       </div>
+      {/* End Latest Courses */}
+      {/* Popular Courses */}
+      <h3 className="pb-1 mb-4 mt-5">Popular Courses <Link to="/popular-courses" className="float-end btn btn-primary">See All</Link></h3>
+      <div className="row mb-4">
+      {popularcourseData && popularcourseData.map((row,index)=>
+        <div className="col-md-3">
+          <div className="card">
+            <Link to={`/detail/${row.course.id}`}><img src={row.course.featured_img} className="card-img-top" alt={row.course.title} /></Link>
+            <div className="card-body">
+            <h5 className="card-title"><Link to={`/detail/${row.course.id}`}>{row.course.title}</Link></h5>
+            </div>
+            <div className='card-footer'>
+              <div className="title">
+                <span>Rating: {row.rating}/5</span>
+                <span className="float-end">Views: {row.course.course_views}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        )}
+      </div>
+      {/* End Popular Courses */}
+      {/* Popular Teachers */}
+      <h3 className="pb-1 mb-4 mt-5">Popular Teachers <Link to="/popular-teachers" className="float-end btn btn-primary">See All</Link></h3>
+      <div className="row mb-4">
+      {popularteacherData && popularteacherData.map((teacher,index)=>
+        <div className="col-md-3">
+          <div className="card">
+          <Link to={`/teacher-detail/${teacher.id}`}><img src={teacher.profile_img} className="card-img-top" alt="..." /></Link>
+            <div className="card-body">
+              <h5 className="card-title"><Link to={`/teacher-detail/${teacher.id}`}>{teacher.full_name}</Link></h5>
+            </div>
+            <div className='card-footer'>
+              <div className="title">
+                <span>Total Courses: {teacher.total_teacher_courses}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+)}
+      </div>
+      {/* End Popular Teachers */}
+      {/* Student Testimonial */}
+      <h3 className="pb-1 mb-4 mt-5">Student Testimonial</h3>
+      <div id="carouselExampleIndicators" className="carousel slide bg-dark text-white py-5" data-bs-ride="carousel">
+        <div className="carousel-indicators">
+        { testimonialData && testimonialData.map((row,index)=>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={index} className={index === 0 ? "active" : "" }></button>
+        )}
+        </div>
+        <div className="carousel-inner">
+        { testimonialData && testimonialData.map((row,i)=>
+          <div className={i === 0 ? "carousel-item text-center active" : "carousel-item text-center" }>
+            <figure class="text-center">
+              <blockquote class="blockquote">
+                <p>{row.reviews}</p>
+              </blockquote>
+              <figcaption class="blockquote-footer">
+                {row.course.title} <cite title="Source Title">{row.student.full_name}</cite>
+              </figcaption>
+            </figure>
+          </div>
+        )}
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+      {/* End Popular Teachers */}
     </div>
   );
 }
